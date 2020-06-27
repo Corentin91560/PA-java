d01c7c5def283fd886ae43d6314682c3bbbf2922
coren <corentin.delage@hotmail.fr>
2020-06-27 18:07:32 +0200

    validate button + stat page 100%

--- src/sample/api/ApiCaller.java
+++ src/sample/api/ApiCaller.java
@@ -1,13 +1,16 @@
 package sample.api;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
+import sample.Class.Feedback;
 import sample.Class.User;
 
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class ApiCaller {
     private String apiPath = "http://localhost:3000/";
@@ -23,6 +26,39 @@ public class ApiCaller {
 
     private ApiCaller(){
 
+    }
+
+    public String validatebug(Feedback feedback){
+        try{
+            String jsonInputString = "{\"idfe\": "+feedback.getIdfe()+"}";
+            System.out.println(jsonInputString);
+            URL url = new URL(apiPath+"feedbackvalide");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            conn.setConnectTimeout(5000);
+            conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.setRequestMethod("POST");
+
+            OutputStream os = conn.getOutputStream();
+            os.write(jsonInputString.getBytes("UTF-8"));
+            os.close();
+            System.out.println(conn.getResponseCode());
+            if (conn.getResponseCode()==200 ){
+                return "ok";
+            }else {
+                return "server "+conn.getResponseCode();
+            }
+
+
+        } catch (MalformedURLException e) {
+            System.out.println(e);
+            return "server error2";
+        } catch (IOException e) {
+            System.out.println(e);
+            return "server error3";
+        }
     }
 
     public User signInUser(User user){
@@ -75,4 +111,151 @@ public class ApiCaller {
         }
         return null;
     }
+    public List<Feedback> Returnbug(){
+        List<Feedback> bugList = new ArrayList<>();
+        try {
+
+            URL url = new URL(apiPath+"feedback/1");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+
+            if (conn.getResponseCode()==200 ){
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(conn.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+                //print response
+                JSONArray jsonArray = new JSONArray(response.toString());
+
+                if (jsonArray != null) {
+                    int len = jsonArray.length();
+                    for (int i=0;i<len;i++){
+                        bugList.add(new Feedback(jsonArray.getJSONObject(i).getInt("idfe"),
+                                jsonArray.getJSONObject(i).getString("title"),
+                                jsonArray.getJSONObject(i).getString("content"),
+                                jsonArray.getJSONObject(i).getString("date"),
+                                jsonArray.getJSONObject(i).getString("status"),
+                                jsonArray.getJSONObject(i).getString("plateform")));
+                    }
+                }
+                conn.disconnect();
+
+                return bugList;
+            }else{
+                System.out.println("buglist error");
+                return bugList;
+            }
+
+
+        } catch (MalformedURLException e) {
+            System.out.println("malformated"+e);
+            return bugList;
+        } catch (IOException e) {
+            System.out.println("io exception"+e);
+            return bugList;
+        }
+    }
+    public List<Feedback> Returnimprove(){
+        List<Feedback> improveList = new ArrayList<>();
+        try {
+
+            URL url = new URL(apiPath+"feedback/2");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+
+            if (conn.getResponseCode()==200 ){
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(conn.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+                //print response
+                JSONArray jsonArray = new JSONArray(response.toString());
+
+                if (jsonArray != null) {
+                    int len = jsonArray.length();
+                    for (int i=0;i<len;i++){
+                        improveList.add(new Feedback(jsonArray.getJSONObject(i).getInt("idfe"),
+                                jsonArray.getJSONObject(i).getString("title"),
+                                jsonArray.getJSONObject(i).getString("content"),
+                                jsonArray.getJSONObject(i).getString("date"),
+                                jsonArray.getJSONObject(i).getString("status"),
+                                jsonArray.getJSONObject(i).getString("plateform")));
+                    }
+                }
+                conn.disconnect();
+
+                return improveList;
+            }else{
+                System.out.println("buglist error");
+                return improveList;
+            }
+
+
+        } catch (MalformedURLException e) {
+            System.out.println("malformated"+e);
+            return improveList;
+        } catch (IOException e) {
+            System.out.println("io exception"+e);
+            return improveList;
+        }
+    }
+    public String BugToTrello(User admin,Feedback feedback){
+
+        String key = "",token = "";
+
+        if (admin.getLogin().equals("admin_01")){
+             key = "2c976bc121a7f659228a9b247acae3f3";
+             token = "f28c03481c79f1558dc0fbc5d5b5268edb532a54ee97f9d179d79c35cf94a1d4";
+        }
+        if (admin.getLogin()=="admin_02"){
+             key = "4d2084403ff7b6e3eaaddba24c4524d3";
+             token = "428e4779cb7c9480f9076faafb5652ebb7e34130cace3074bee458a2e62a606c";
+        }
+        if (admin.getLogin()=="admin_03"){
+             key = "";
+             token = "";
+        }
+
+        try{
+            String jsonInputString = "{\"appli\": \""+feedback.getPlateform()+"\", \"idfe\": \""+feedback.getIdfe()+"\", \"key\": \""+key+"\",\"token\": \""+token+"\", \"name\": \""+feedback.getTitle()+"\", \"desc\": \""+feedback.getContent()+"\"}";
+            System.out.println(jsonInputString);
+            URL url = new URL(apiPath+"trello/feedback");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+
+            conn.setConnectTimeout(5000);
+            conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.setRequestMethod("POST");
+
+            OutputStream os = conn.getOutputStream();
+            os.write(jsonInputString.getBytes("UTF-8"));
+            os.close();
+            System.out.println(conn.getResponseCode());
+            if (conn.getResponseCode()==200 ){
+                return "ok";
+            }else {
+                return "server error1";
+            }
+
+
+        } catch (MalformedURLException e) {
+            System.out.println(e);
+            return "server error2";
+        } catch (IOException e) {
+            System.out.println(e);
+            return "server error3";
+        }
+        }
 }
--- src/sample/controller/HomeController.java
+++ src/sample/controller/HomeController.java
@@ -1,17 +1,57 @@
 package sample.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-import javafx.stage.Stage;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import sample.Class.Feedback;
+import sample.Class.User;
+import sample.api.ApiCaller;
 
-import java.io.IOException;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
 
 public class HomeController {
+
+    List<Feedback> listimprove;
+    int selected;
+    List<Feedback> listbug;
+    User currentUser = LoginController.currentUser;
+    @FXML
+    private ListView<String> buglist;
+    @FXML
+    private ListView<String> improvelist;
+    @FXML
+    private TextField tftitlebug;
+    @FXML
+    private TextArea tfcontentbug;
+    @FXML
+    private TextField tfstatusbug;
+    @FXML
+    private TextField tfplatebug;
+    @FXML
+    private TextField tftitleimp;
+    @FXML
+    private TextArea tfcontentimp;
+    @FXML
+    private TextField tfplateimp;
+    @FXML
+    private Button btnaddtrello;
+    @FXML
+    private Button btnvalidate;
+
+    private ObservableList<String> observableBugList = FXCollections.observableArrayList();
+    private ObservableList<String> observableImproveList = FXCollections.observableArrayList();
+
+
+    public void initialize(){
+        refresh();
+    }
+
     public void Disconnect(ActionEvent actionEvent) {
 
 
@@ -31,5 +71,100 @@ public class HomeController {
             alert.close();
         }
 
+    }
+
+    public void refresh(){
+
+        ApiCaller caller = ApiCaller.getInstance();
+        listbug = caller.Returnbug();
+        int i=0;
+        observableBugList.clear();
+        while(i<listbug.size()){
+
+            observableBugList.add(listbug.get(i).getDate().substring(0,10)+" - "+listbug.get(i).getTitle());
+            i=i+1;
+        }
+
+        Comparator<String> comparator = Comparator.comparing(String::new);
+        comparator = comparator.reversed();
+        FXCollections.sort(observableBugList,comparator);
+
+        buglist.setItems(observableBugList);
+        buglist.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+            @Override
+            public void handle(MouseEvent event) {
+                System.out.println("clicked on " + buglist.getSelectionModel().getSelectedIndices());
+                selected = buglist.getSelectionModel().getSelectedIndices().get(0);
+                tfcontentbug.setText(listbug.get(selected).getContent());
+                tfstatusbug.setText(listbug.get(selected).getStatus());
+                if (listbug.get(selected).getStatus().equals("pending") || listbug.get(selected).getStatus().equals("validate")){
+                    btnaddtrello.setDisable(true);
+                    btnvalidate.setDisable(false);
+                }else{
+                    btnaddtrello.setDisable(false);
+                    btnvalidate.setDisable(true);
+                }
+                tftitlebug.setText(listbug.get(selected).getTitle()+" #"+listbug.get(selected).getIdfe());
+                tfplatebug.setText(listbug.get(selected).getPlateform());
+            }
+        });
+
+        listimprove = caller.Returnimprove();
+        int j=0;
+        observableImproveList.clear();
+        while(j<listimprove.size()){
+            observableImproveList.add(listimprove.get(j).getTitle());
+            j=j+1;
+        }
+        improvelist.setItems(observableImproveList);
+
+        improvelist.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+            @Override
+            public void handle(MouseEvent event) {
+                System.out.println("clicked on " + improvelist.getSelectionModel().getSelectedItem());
+                selected = improvelist.getSelectionModel().getSelectedIndices().get(0);
+                //System.out.println(listbug.get(selected));
+                tfcontentimp.setText(listimprove.get(selected).getContent());
+                tftitleimp.setText(listimprove.get(selected).getTitle()+" #"+listimprove.get(selected).getIdfe());
+                tfplateimp.setText(listimprove.get(selected).getPlateform());
+            }
+        });
+    }
+
+
+    public void AddBugToTrello(ActionEvent actionEvent) {
+        ApiCaller caller = ApiCaller.getInstance();
+        String information = caller.BugToTrello(currentUser,listbug.get(selected));
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        System.out.println(actionEvent);
+        alert.setTitle("Information Dialog");
+        alert.setHeaderText(information);
+        alert.showAndWait();
+        refresh();
+    }
+
+    public void AddImpToTrello(ActionEvent actionEvent) {
+
+
+
+    }
+    public void ValidateBug(ActionEvent actionEvent) {
+        ApiCaller caller = ApiCaller.getInstance();
+        String information = caller.validatebug(listbug.get(selected));
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        System.out.println(actionEvent);
+        alert.setTitle("Information Dialog");
+        alert.setHeaderText(information);
+        alert.showAndWait();
+        refresh();
+
+
+    }
+
+    public void GoToStat(ActionEvent actionEvent) {
+        ChangeSceneController controller = new ChangeSceneController();
+        controller.changeScene("../ressource/stat.fxml", actionEvent);
     }
 }
